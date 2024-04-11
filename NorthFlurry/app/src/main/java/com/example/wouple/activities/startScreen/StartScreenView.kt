8f8d39663e090ea16043cff7df06b91c24b41d0a
EmreRuy <uyar.em.eu@gmail.com>
2024-04-11 23:57:35 +0300

    commit

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/startScreen/StartScreenView.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/startScreen/StartScreenView.kt
@@ -22,6 +22,7 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
