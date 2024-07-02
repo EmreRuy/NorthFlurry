724d933fe276ed82c2486e2e4012b135fc02fb1d
Ida Kristine Riseng <ida.kristine.riseng@posten.no>
2024-07-02 22:11:25 +0200

    Update MainActivity.kt

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/mainActivity/MainActivity.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/mainActivity/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -73,7 +74,7 @@ class MainActivity : ComponentActivity() {
                         if(temp.value == null){
                             LoadingScreen()
                         }else{
-                            displayFirstCardView(activity = this)
+                            DisplayFirstCardView(activity = this)
                         }
                     }
                 }
@@ -81,8 +82,8 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun displayFirstCardView(activity: ComponentActivity) {
-        setContent {
+    @Composable
+    private fun DisplayFirstCardView(activity: ComponentActivity) {
             val isConnected by remember { mutableStateOf(true) }
             var isLoading by remember { mutableStateOf(true) }
             LaunchedEffect(true) {
@@ -134,7 +135,7 @@ class MainActivity : ComponentActivity() {
                         }
                     )
                 }
-            }
+
         }
     }
 
