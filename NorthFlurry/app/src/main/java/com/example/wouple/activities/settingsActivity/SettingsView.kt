35b8ef14f55d1ad13c6688a0d5b006b0d23f1106
EmreRuy <uyar.em.eu@gmail.com>
2024-12-16 15:20:43 +0300

    started again

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/settingsActivity/SettingsView.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/settingsActivity/SettingsView.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
@@ -40,8 +42,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
+import androidx.compose.ui.graphics.Color.Companion.Unspecified
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.wouple.R
 import com.example.wouple.activities.mainActivity.components.LottieAnimationShootingStar
@@ -89,13 +94,23 @@ fun SettingsView(
         topBar = {
             TopAppBar(
                 title = {
-                    Text(
-                        modifier = Modifier.padding(start = 4.dp, top = 2.dp),
-                        text = stringResource(id = R.string.Settings),
-                        fontWeight = FontWeight.Light
-                    )
+                    Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically) {
+                        Spacer(modifier = Modifier.padding(start = 24.dp))
+                        Icon(
+                            modifier = Modifier.size(32.dp),
+                            painter = painterResource(id = R.drawable.logo2),
+                            contentDescription = null,
+                            tint = Unspecified
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Text(
+                            modifier = Modifier.padding(start = 40.dp, top = 2.dp),
+                            text = stringResource(id = R.string.app_name),
+                            fontWeight = FontWeight.Thin,
+                        )
+                    }
                 },
-                navigationIcon = {
+              /*  navigationIcon = {
                     IconButton(
                         onClick = onBackPressed
                     ) {
@@ -105,7 +120,7 @@ fun SettingsView(
                             modifier = Modifier.size(28.dp)
                         )
                     }
-                },
+                }, */
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = Transparent,
                     navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
