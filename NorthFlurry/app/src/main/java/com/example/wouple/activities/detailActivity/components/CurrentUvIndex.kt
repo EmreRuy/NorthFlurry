8069a63bfc095498030d74f8e98af7993f4bdd73
EmreRuy <uyar.em.eu@gmail.com>
2025-05-01 16:42:56 +0300

    UV index in circleProgBar

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/detailActivity/components/CurrentUvIndex.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/detailActivity/components/CurrentUvIndex.kt
@@ -1,28 +1,16 @@
 package com.example.wouple.activities.detailActivity.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.wouple.R
@@ -32,66 +20,42 @@ import java.time.ZonedDateTime
 
 @Composable
 fun CurrentUvIndex(temp: TemperatureResponse) {
-    Column(
-        modifier = Modifier
-            .padding(vertical = 8.dp, horizontal = 16.dp)
-           // .shadow(elevation = 2.dp, shape = RoundedCornerShape(20.dp))
-            .background(MaterialTheme.colorScheme.secondaryContainer, shape = RoundedCornerShape(20.dp))
-            .padding(12.dp),
-    ) {
-        UvIndex(temp)
-    }
-}
+    val timeZone = temp.timezone
+    val currentDateTime = ZonedDateTime.now(ZoneId.of(timeZone))
+    val currentHour = currentDateTime.hour
+    val uvIndexValue = temp.hourly.uv_index[currentHour].toFloat().coerceAtMost(11f)
 
-@Composable
-fun UvIndex(temp: TemperatureResponse) {
     Column(
         modifier = Modifier
             .padding(horizontal = 20.dp)
             .background(Color.Transparent),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Icon(
-                modifier = Modifier
-                    .padding(end = 4.dp)
-                    .size(20.dp),
-                painter = painterResource(id = R.drawable.ic_sun),
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-            Text(
-                modifier = Modifier.padding(4.dp),
-                text = stringResource(id = R.string.current_uv_index).uppercase(),
-                textAlign = TextAlign.Start,
-                color = MaterialTheme.colorScheme.onSurfaceVariant,
-                fontWeight = FontWeight.Medium,
-                fontFamily = FontFamily.SansSerif
-            )
-            Spacer(modifier = Modifier.weight(0.5f))
-            val timeZone = temp.timezone
-            val currentDateTime = ZonedDateTime.now(ZoneId.of(timeZone))
-            val currentHour = currentDateTime.hour
-            val aqiValue = temp.hourly.uv_index[currentHour].toInt().toString()
-            Text(
-                modifier = Modifier.padding(end = 10.dp),
-                text = aqiValue,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Light,
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-            val uvIndexDescriptions =
-                getUvIndexDescription(uvIndex = temp.hourly.uv_index[currentHour].toInt())
-            Text(
-                modifier = Modifier.padding(start = 4.dp),
-                text = uvIndexDescriptions,
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Light,
-                color = MaterialTheme.colorScheme.onPrimaryContainer
-            )
-        }
+        Text(
+            text = stringResource(id = R.string.current_uv_index).uppercase(),
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        CircularProgressBar(
+            percentage = uvIndexValue / 11f, // max UV index scale is  0–11+
+            number = uvIndexValue.toInt(),
+            fontSize = 24.sp,
+            radius = 60.dp,
+            color = MaterialTheme.colorScheme.primary,
+            backgroundColor = MaterialTheme.colorScheme.secondary.copy(alpha = 0.3f),
+            strokeWidth = 10.dp
+        )
+
+        val uvIndexDescriptions = getUvIndexDescription(uvIndexValue.toInt())
+
+        Text(
+            text = uvIndexDescriptions,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Light,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            modifier = Modifier.padding(top = 8.dp)
+        )
     }
 }
