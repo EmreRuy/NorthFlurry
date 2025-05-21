e13b8c1f4cd1ca8d78371f0e861b8c0e69089ac7
EmreRuy <uyar.em.eu@gmail.com>
2025-05-21 15:52:38 +0300

    DayLightDurationCard
    New card design

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/detailActivity/components/DayLightDuration.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/detailActivity/components/DayLightDuration.kt
@@ -6,10 +6,7 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,11 +22,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
@@ -224,7 +218,7 @@ fun getDayLengthString(temp: TemperatureResponse): String {
         val hours = duration.toHours()
         val minutes = duration.toMinutes() % 60
         "${hours}h ${minutes}m"
-    } catch (e: Exception) {
+    } catch (_: Exception) {
         "N/A"
     }
 }
