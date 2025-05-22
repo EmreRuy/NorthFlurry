c7ec4e827b10d743059acb67f974294b67729286
EmreRuy <uyar.em.eu@gmail.com>
2025-05-22 22:07:27 +0300

    debug attempt on daylight duration

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/detailActivity/components/DayLightDuration.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/detailActivity/components/DayLightDuration.kt
@@ -1,5 +1,6 @@
 package com.example.wouple.activities.detailActivity.components
 
+import android.util.Log
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
@@ -22,6 +23,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
