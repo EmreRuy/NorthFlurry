221c30b8af8fbeb58a9c84c5289fbd1367f21849
EmreRuy <uyar.em.eu@gmail.com>
2025-06-19 20:54:39 +0300

    Updates

--- NorthFlurry/app/src/main/java/com/example/wouple/activities/mainActivity/components/SevenHoursForecast.kt
+++ NorthFlurry/app/src/main/java/com/example/wouple/activities/mainActivity/components/SevenHoursForecast.kt
@@ -1,6 +1,5 @@
 package com.example.wouple.activities.mainActivity.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,20 +8,12 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.wouple.elements.GetHourlyWeatherInfo
 import com.example.wouple.elements.SevenHoursCardNotification
 import com.example.wouple.model.api.TemperatureResponse
-import kotlinx.coroutines.delay
 
 @Composable
 fun GetSevenHoursForecast(temp: TemperatureResponse) {
